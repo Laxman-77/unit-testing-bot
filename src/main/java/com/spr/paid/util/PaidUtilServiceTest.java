8368863c4e614c5c17f107698ef9a7ec42f5b424
Tarun Kumar <tarun.janamaddi@sprinklr.com>
2021-07-20 09:56:33 +0530

    Added unit tests

--- src/main/java/com/spr/paid/util/PaidUtilServiceTest.java
+++ src/main/java/com/spr/paid/util/PaidUtilServiceTest.java
@@ -3,6 +3,16 @@ package com.spr.paid.util;
 import org.junit.Test;
 
 public class PaidUtilServiceTest {
+    @Test
+    public void testAppInstallCTA() {
+    }
 
+    @Test
+    public void testConversionCTA() {
+    }
+
+    @Test
+    public void testPostEngagementCTA() {
+    }
 
 }
