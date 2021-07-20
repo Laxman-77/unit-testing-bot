200e993ad804f0faa6fb6b6fe621388454888227
Tarun Kumar <tarun.janamaddi@sprinklr.com>
2021-07-20 13:14:51 +0530

    Added unit tests

---
+++ src/main/java/com/spr/modules/advocacy/ContentListServiceTest.java
@@ -0,0 +1,22 @@
+package com.spr.modules.advocacy;
+
+import org.junit.Test;
+
+public class ContentListServiceTest {
+
+    @Test
+    public void testSearchPinnedAssets(){
+    }
+
+    @Test
+    public void testSearchPinnedAssetsFromES(){
+    }
+
+    @Test
+    public void testSearchPinnedExpiredAssetsFromES(){
+    }
+
+    @Test
+    public void testSearchPinnedPublishedAssetsFromES(){
+    }
+}
